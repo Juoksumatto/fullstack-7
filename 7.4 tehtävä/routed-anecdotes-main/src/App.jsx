10d007c9db56bc7571c858f26a19315f0014133b
onni <holttaonni@gmail.com>
2026-08-17 13:05:18 +0300

    Add files via upload

---
+++ 7.4 tehtävä/routed-anecdotes-main/src/App.jsx
@@ -0,0 +1,32 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Menu from './components/Menu'
+import AnecdoteList from './components/AnecdoteList'
+import About from './components/About'
+import Footer from './components/Footer'
+import CreateNew from './components/CreateNew'
+import { useAnecdotes } from './hooks'
+
+const App = () => {
+  const { anecdotes, setAnecdotes } = useAnecdotes()
+
+  const addAnecdote = (anecdote) => {
+    setAnecdotes(anecdotes.concat({ ...anecdote, id: Math.round(Math.random() * 10000) }))
+  }
+
+  return (
+    <Router>
+      <div>
+        <h1>Software anecdotes</h1>
+        <Menu />
+        <Routes>
+          <Route path="/" element={<AnecdoteList anecdotes={anecdotes} />} />
+          <Route path="/create" element={<CreateNew addAnecdote={addAnecdote} />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
+  )
+}
+
+export default App
---
+++ 7.4 tehtävä/routed-anecdotes-main/src/hooks.js
@@ -0,0 +1,38 @@
+import { useEffect, useState } from 'react'
+import anecdoteService from './services/anecdotes'
+
+export const useField = (type) => {
+  const [value, setValue] = useState('')
+
+  const onChange = (event) => {
+    setValue(event.target.value)
+  }
+
+  const reset = () => {
+    setValue('')
+  }
+
+  return {
+    input: {
+      type,
+      value,
+      onChange
+    },
+    reset
+  }
+}
+
+export const useAnecdotes = () => {
+  const [anecdotes, setAnecdotes] = useState([])
+
+  useEffect(() => {
+    anecdoteService
+      .getAll()
+      .then((data) => setAnecdotes(data))
+      .catch((error) => {
+        console.error('Failed to fetch anecdotes:', error)
+      })
+  }, [])
+
+  return { anecdotes, setAnecdotes }
+}

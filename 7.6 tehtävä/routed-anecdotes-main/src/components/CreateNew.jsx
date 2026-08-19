c365666b57cf498dcfb026dfe058a3507fc50cd4
onni <holttaonni@gmail.com>
2026-08-19 08:31:35 +0300

    Add files via upload

---
+++ 7.6 tehtävä/routed-anecdotes-main/src/components/CreateNew.jsx
@@ -0,0 +1,57 @@
+import { useNavigate } from 'react-router-dom'
+import { useAnecdotes, useField } from '../hooks'
+
+const CreateNew = () => {
+  const content = useField('text')
+  const author = useField('text')
+  const info = useField('text')
+  const { addAnecdote } = useAnecdotes()
+  const navigate = useNavigate()
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    
+    await addAnecdote({ 
+      content: content.input.value,
+      author: author.input.value,
+      info: info.input.value,
+      votes: 0,
+    })
+    
+    navigate('/')
+
+  }
+
+  return (
+    <div>
+      <h2>create a new anecdote</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          content
+          <input {...content.input} />
+        </div>
+        <div>
+          author
+          <input {...author.input} />
+        </div>
+        <div>
+          url for more info
+          <input {...info.input} />
+        </div>
+        <button>create</button>
+        <button
+          type="button"
+          onClick={() => {
+            content.reset()
+            author.reset()
+            info.reset()
+          }}
+        >
+          reset
+        </button>
+      </form>
+    </div>
+  )
+}
+
+export default CreateNew
---
+++ 7.6 tehtävä/routed-anecdotes-main/src/hooks.js
@@ -0,0 +1,51 @@
+import { useEffect, useState } from 'react'
+import anecdoteService from './services/anecdotes'
+
+export const useField = (type) => {
+  const [value, setValue] = useState('')
+
+  const onChange = (event) => {
+    setValue(event.target.value)
+  }
+
+  const reset = () => {
+    setValue('')
+  }
+
+  return {
+    input: {
+      type,
+      value,
+      onChange
+    },
+    reset
+  }
+}
+
+export const useAnecdotes = () => {
+  const [anecdotes, setAnecdotes] = useState([])
+
+  useEffect(() => {
+    anecdoteService
+      .getAll()
+      .then((data) => setAnecdotes(data))
+      .catch((error) => {
+        console.error('Failed to fetch anecdotes:', error)
+      })
+  }, [])
+
+  const addAnecdote = async (anecdote) => {
+    const createdAnecdote = await anecdoteService.createNew(anecdote)
+    setAnecdotes((prevAnecdotes) => prevAnecdotes.concat(createdAnecdote))
+    return createdAnecdote
+  }
+
+  const deleteAnecdote = async (id) => {
+    await anecdoteService.deleteAnecdote(id)
+    setAnecdotes((prevAnecdotes) => (
+      prevAnecdotes.filter((anecdote) => anecdote.id !== id)
+    ))
+  }
+
+  return { anecdotes, addAnecdote, deleteAnecdote }
+}

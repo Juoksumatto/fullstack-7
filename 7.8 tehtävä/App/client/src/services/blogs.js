5958b8e67316a5b4fa473e153b2b1d124523e105
onni <holttaonni@gmail.com>
2026-08-21 08:49:32 +0300

    Add files via upload

---
+++ 7.8 tehtävä/App/client/src/services/blogs.js
@@ -0,0 +1,45 @@
+import axios from 'axios'
+const baseUrl = '/api/blogs'
+let token = null
+
+const setToken = newToken => {
+  token = newToken ? `Bearer ${newToken}` : null
+}
+
+const getAll = () => {
+
+  const request = axios.get(baseUrl)
+  return request.then(response => response.data)
+}
+
+const create = async newObject => {
+  const config = {
+    headers: { Authorization: token }
+  }
+
+  const response = await axios.post(baseUrl, newObject, config)
+  return response.data
+}
+
+const update = (id, newObject) => {
+  const request = axios.put(`${ baseUrl }/${id}`, newObject)
+  return request.then(response => response.data)
+}
+
+const remove = async (id) => {
+  const config = {
+    headers: { Authorization: token }
+  }
+  const request = await axios.delete(`${ baseUrl }/${id}`, config)
+  return request.data
+}
+
+const like = async (id) => {
+  const blog = await getAll().then(blogs => blogs.find(b => b.id === id))
+  if (!blog) throw new Error('Blog not found')
+
+  const updatedBlog = { ...blog, likes: blog.likes + 1 }
+  return update(id, updatedBlog)
+}
+
+export default { getAll, setToken, create, update, like, remove }
